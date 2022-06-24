f426f5319b09605b376cad1eb8d4aa091c18961d
huy <kyhuytram@gmail.com>
2022-06-24 07:10:56 +0700

    update 24/6/22

--- index.php
+++ index.php
@@ -83,7 +83,17 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
 <script>
     jQuery(window).scroll(function(){
         var vscroll = jQuery(this).scrollTop();
-        console.log(vscroll);
+        jQuery('#logotext').css({
+            "transform" : "translate(0px, "+vscroll/2+"px)"
+        });
+        var vscroll = jQuery(this).scrollTop();
+        jQuery('#back-flower').css({
+            "transform" : "translate("+vscroll/5+"px, -"+vscroll/12+"px)"
+        });
+        var vscroll = jQuery(this).scrollTop();
+        jQuery('#fore-flower').css({
+            "transform" : "translate(0px, -"+vscroll/2+"px)"
+        });
     });
 </script>
 </body>
